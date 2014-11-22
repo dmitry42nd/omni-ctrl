2f217e8b04b259c51b84e364bf8dbaf45974a12f
dmitry <dmitry42nd@gmail.com>
2014-11-22 15:06:47 +0300

    manual calibrating tool added

--- include/segway.h
+++ include/segway.h
@@ -14,7 +14,10 @@ public:
 
 private slots:
   void onBtnPressed(int code, int state);
-
+  void onGamepadPadDown(int pd ,int x, int y);
+  void onGamepadPadUp(int pd);
+  void onGamepadBtnChanged(int code, int state);
+  
   void startDriftAcc();
   void stopDriftAcc();
   void startDancing();
@@ -33,6 +36,19 @@ private:
   double m_gyroData;
   double m_outData;
   double m_outDataOld;
+
+/*
+  1 - pk, dk
+  2 - pk, ik
+  3 - rowrow
+*/
+  double m_rowrow;
+  double m_wewwew;
+  int m_state;  
+  
+  double m_pk;
+  double m_dk;
+  double m_ik;
 
   double m_offset;  
   int m_gyroGain;
--- src/segway.cpp
+++ src/segway.cpp
@@ -10,11 +10,16 @@ const QString cmdFifoPath="/tmp/dsp-detector.in.fifo";
 const QString l = "M3";
 const QString r = "M4";
 
+
+const double pk = 8.5;
+const double dk = 12;
+const double ik = 1;
+
 const int gyroAxis = 0;
 const int acceAxis = 2;
 
 const double G = 4096;
-const double K = 0.025; //0.02 
+const double K = 0.02; //0.02 
 
 const int gdcPeriod  = 4000;
 const int mainPeriod = 10;
@@ -35,14 +40,62 @@ Segway::Segway(QThread *guiThread, QString configPath, QString startDirPath):
   m_brick(*guiThread, configPath, startDirPath),
   m_outData(),
   m_outDataOld(),
-  m_offset()
+  m_offset(2.9),
+  m_state(1),
+  m_pk(6.5),
+  m_dk(11.2),
+  m_ik(0.2),
+  m_rowrow(0)
 {
   qDebug() << "SEGWAY_STARTS";
   connect(m_brick.keys(), SIGNAL(buttonPressed(int,int)), this, SLOT(onBtnPressed(int,int)));
+  
+  connect(m_brick.keys(), SIGNAL(buttonPressed(int,int)), this, SLOT(onBtnPressed(int,int)));
+  
+  connect(m_brick.gamepad(), SIGNAL(button(int,int)), this, SLOT(onGamepadBtnChanged(int, int)));
+  connect(m_brick.gamepad(), SIGNAL(pad(int,int,int)), this, SLOT(onGamepadPadDown(int,int,int)));
+  connect(m_brick.gamepad(), SIGNAL(padUp(int)),       this, SLOT(onGamepadPadUp(int)));
 
   startDriftAcc();
   QTimer::singleShot(gdcPeriod     , this, SLOT(stopDriftAcc()));
   QTimer::singleShot(gdcPeriod + 10, this, SLOT(startDancing()));
+}
+
+
+void Segway::onGamepadPadDown(int pd ,int x, int y) 
+{
+  if(m_state == 3)
+    if(pd == 1)
+      m_rowrow = y/50.0;
+      m_wewwew = x/10.0;
+  else {
+    if (pd == 1)
+      m_pk += x >= 0 ? 0.1 : -0.1;
+    else //if (pd == 2)
+      switch(m_state) {
+        case 1: m_dk += x >= 0 ? 0.1 : -0.1; break;
+        case 2: m_ik += x >= 0 ? 0.1 : -0.1; break;
+      }
+  }
+
+
+void Segway::onGamepadPadUp(int pd) 
+{
+  if(pd == 1)
+    m_rowrow = 0;
+    m_wewwew = 0;
+}
+
+void Segway::onGamepadBtnChanged(int code, int state)
+{
+  if (state == 0) return;
+  
+  switch(code) {
+    case 1 : m_state = 1; break;
+    case 2 : m_state = 2; break;
+    case 3 : m_state = 3; break;
+    default : break;
+  }
 }
 
 void Segway::onBtnPressed(int code, int state)
@@ -50,8 +103,9 @@ void Segway::onBtnPressed(int code, int state)
   if(state == 0) return;
   
   switch(code) {
+    case 104:
+      m_offset = 0;
     case 105:
-      qDebug() << "pew";
       m_offset = m_outData;
     default : break;
   }
@@ -94,26 +148,22 @@ void Segway::startDancing()
   m_mainTicker.start(mainPeriod);
 }
 
-const double pk = 4.5;
-const double dk = 5;
-const double ik = 1;
-
 void Segway::dance()
 {
   int gyroData = m_brick.gyroscope()->read()[gyroAxis] - m_gyroDrift;
   int acceData = m_brick.accelerometer()->read()[acceAxis];
  
   m_gyroData = gyroData*parToDeg*mainPeriodS;
-  m_acceData  = asin(sat(acceData/G, 1))*180/3.14159;
+  m_acceData  = acceData*(180/3.14159*G);
   
-  m_outData = ((1-K)*(m_outData+ m_gyroData) + K*m_acceData);
-  int yaw  = (sgn(m_outData)*minPow + 2*m_outData*pk + (m_outData-m_outDataOld)*dk + (m_outData+m_outDataOld)*ik);
-  m_outDataOld = m_outData;
+  m_outData = ((1-K)*(m_outData + m_gyroData) + K*m_acceData);
+  double tmp = m_outData - m_offset;
+  double tmp2 = tmp + m_rowrow;
+  int yaw  = 2*(tmp2*m_pk + (tmp2-m_outDataOld)*m_dk + (tmp2+m_outDataOld)*ik);
+  m_outDataOld = tmp;
 
-  m_brick.motor(l)->setPower(yaw);
-  m_brick.motor(r)->setPower(yaw);
+  m_brick.motor(l)->setPower(yaw+m_wewwew);
+  m_brick.motor(r)->setPower(yaw-m_wewwew);
 
-  
-  qDebug() << m_outData << " " << yaw;
+  qDebug("data yaw: %1.5f %d pdi: %1.1f %1.1f %1.1f rr: %1.2f\n", tmp, yaw, m_pk, m_dk, m_ik,m_rowrow);
 }
-
